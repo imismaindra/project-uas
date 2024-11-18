f2d895134b3ca6d67b693b6d5c213f5ff34be4b6
Ismaindra <imismaindra@gmail.com>
2024-11-18 11:46:22 +0700

    added category slug input field to category insert form

--- views/category_insert.php
+++ views/category_insert.php
@@ -47,6 +47,17 @@
                             required>
                         </textarea>
                     </div>
+                    <div>
+                        <label for="slug" class="block mb-2 text-sm font-medium text-gray-900">Category slug</label>
+                        <input 
+                            type="text" 
+                            id="slug" 
+                            name="slug" 
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                            placeholder="Enter category slug" 
+                            required 
+                        />
+                    </div>
                 </div>
 
                 <!-- Submit Button -->
